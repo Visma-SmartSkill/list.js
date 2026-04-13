1e139efefb46f97ed89d614ed4fb89c6ddbfa53f
Odd Skancke <odd.skancke@visma.com>
2026-04-13 08:44:35 +0200

    feat: optionally remember the last search term

--- src/index.js
+++ src/index.js
@@ -34,6 +34,7 @@ module.exports = function (id, options, values) {
       self.matchingItems = []
       self.searched = false
       self.filtered = false
+      self.rememberLastSearch = false
       self.searchColumns = undefined
       self.searchDelay = 0
       self.handlers = { updated: [] }
