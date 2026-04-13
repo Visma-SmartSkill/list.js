5298cd58fe251d79fab631b0c0c6bdf63356a1c2
Odd Skancke <odd.skancke@visma.com>
2026-04-13 08:45:45 +0200

    Feat: Make escaping regex chars optional

--- src/index.js
+++ src/index.js
@@ -34,6 +34,7 @@ module.exports = function (id, options, values) {
       self.matchingItems = []
       self.searched = false
       self.filtered = false
+      self.escapeRegexChars = false
       self.rememberLastSearch = false
       self.searchColumns = undefined
       self.searchDelay = 0
